b2c1f16441acd64221367874e13a6305fef7da1c
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-26 15:32:48 -0500

    building toggle light and dark page mode

--- iris/src/App.jsx
+++ iris/src/App.jsx
@@ -1,14 +1,27 @@
 import './App.scss'
 import HomePage from './pages/HomePage/HomePage';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 function App() {
+  const [mode, setMode] = useState(localStorage.getItem("mode") || "light" );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-mode", mode);
+    localStorage.setItem("mode", mode); 
+  }, [mode]);
+  
+  const handleModeToggle = () => {
+    const newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+  };
   return (
     <>
     <BrowserRouter>
+   
         <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={<WelcomePage handleModeToggle={handleModeToggle} />} />
           <Route path="/home" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
--- iris/src/pages/WelcomePage/WelcomePage.jsx
+++ iris/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,19 +1,24 @@
 import "./WelcomePage.scss";
+import smileRain from "/src/assets/images/single-smile-rain.svg"
 import { useNavigate } from "react-router-dom";
 
-const WelcomePage = () => {
+const WelcomePage = ({handleModeToggle}) => {
   const navigate = useNavigate();
 
   function clickHandler(e) {
     navigate("/home");
   }
+
+
   return (
     <main>
-      <div className="welcome">
+      <div className="welcome" data-theme='dark'>
+        <img src={smileRain} className="welcome__image"/>
         <h1 className="welcome__heading">Welcome!</h1>
         <button className="welcome__button" onClick={clickHandler}>
           Get Started
         </button>
+        <p className="mode-text">Set the Mood:</p>
       </div>
     </main>
   );
