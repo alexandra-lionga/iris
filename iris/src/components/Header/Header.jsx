1ed350b18cdc499af924b44004d693bbf51cb14f
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-26 10:06:59 -0500

    creating header component and global styles

---
+++ iris/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+import logo from "/src/assets/logo/iris-logo.svg"
+
+function Header(){
+    <header>
+        <nav>
+            <img src={logo} alt="website logo"/>
+        </nav>
+    </header>
+
+}
+
+export default Header;
