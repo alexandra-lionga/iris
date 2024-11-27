8d20bec51b0979eb11252dea9e52926c977530c5
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-27 13:00:29 -0500

    listing categories in the main page and styling

--- iris/src/components/Main/Main.jsx
+++ iris/src/components/Main/Main.jsx
@@ -1,12 +1,39 @@
 import Quotes from "../Quotes/Quotes";
+import { Link } from "react-router-dom";
 import "./Main.scss";
 
+export const Main = ({ contentList }) => {
+  const categories = [
+    "GoodDeeds",
+    "Innovation",
+    "Health",
+    "Environment",
+    "Humanitarian",
+    "SuccessStories",
+    "Inspiration",
+    "Community",
+    "Kindness",
+    "PositiveEvents",
+  ];
 
-export const Main = () => {
- 
   return (
     <main>
-      <Quotes/>
+      <Quotes />
+      <div className="categories">
+        <h2 className="categories__heading">Categories</h2>
+        <ul className="categories__list">
+          {categories.map((category) => {
+            return (
+              <li
+                key={categories.indexOf(category)}
+                className="categories__list-item"
+              >
+                <Link to={`/category/${category}`} className="categories__link">{category}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 };
